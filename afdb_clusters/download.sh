a805a9bf33d64b753108a2c55bfee83cbef426f9
Cameron Gilchrist <clmgilchrist@gmail.com>
2024-12-18 16:16:40 +0900

    add AA extraction to downloader

--- afdb_clusters/download.sh
+++ afdb_clusters/download.sh
@@ -2,13 +2,16 @@
 
 # Download PDB files from AlphaFold for each extracted cluster
 # Files will be written to <accession>/pdbs/, the expected file structure for align_families.sh
+# Will also produce <accession>/sequence.fa by extracting AA sequences from downlaoded PDB files
 
 while IFS=$'\t' read -r acc cath nummem numdom members
 do
 	echo "# Processing $acc"
-	mkdir -p "${acc}/pdbs"
+	pdbs="${acc}/pdbs"
+	mkdir -p "$pdbs"
 	echo "$members" |\
 		tr "," "\n" |\
 		xargs -I{} echo "https://alphafold.ebi.ac.uk/files/AF-"{}"-F1-model_v4.pdb" |\
 		aria2c -s 20 -d "${acc}/pdbs" -i -
+	find "$pdbs" -type f -name "*.pdb" -exec ./extract_sequences.awk {} \; > "${acc}/sequence.fa"
 done < "$1"
