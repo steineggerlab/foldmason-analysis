8c138d28d75a92e37d34fdac86e33002952ebf98
Cameron Gilchrist <clmgilchrist@gmail.com>
2024-12-18 16:58:00 +0900

    update downloader

--- afdb_clusters/download.sh
+++ afdb_clusters/download.sh
@@ -4,6 +4,8 @@
 # Files will be written to <accession>/pdbs/, the expected file structure for align_families.sh
 # Will also produce <accession>/sequence.fa by extracting AA sequences from downlaoded PDB files
 
+dir=$(dirname $(readlink -f "$0"))
+
 while IFS=$'\t' read -r acc cath nummem numdom members
 do
 	echo "# Processing $acc"
@@ -13,5 +15,5 @@ do
 		tr "," "\n" |\
 		xargs -I{} echo "https://alphafold.ebi.ac.uk/files/AF-"{}"-F1-model_v4.pdb" |\
 		aria2c -s 20 -d "${acc}/pdbs" -i -
-	find "$pdbs" -type f -name "*.pdb" -exec ./extract_sequences.awk {} \; > "${acc}/sequence.fa"
+	find "$pdbs" -type f -name "*.pdb" -exec "${dir}/extract_sequences.awk" {} \; > "${acc}/sequence.fa"
 done < "$1"
