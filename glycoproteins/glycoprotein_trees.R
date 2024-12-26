24fb4a4ad972bb5d5ac5454771cbd9c6f978839f
Cameron Gilchrist <clmgilchrist@gmail.com>
2024-12-26 13:02:23 +0900

    update tree script

--- glycoproteins/glycoprotein_trees.R
+++ glycoproteins/glycoprotein_trees.R
@@ -8,8 +8,47 @@ library(ggtree)
 library(ggplot2)
 library(svglite)
 
-# Finds matching nodes and extract bootstrap values
+
+# Mifsud et al trees
+m_e <- read.tree("/Users/gamcil/repos/foldmason-analysis/glycoproteins/trees/mifsud_denv1_e_mapped.nw")
+m_e1 <- read.tree("/Users/gamcil/repos/foldmason-analysis/glycoproteins/trees/mifsud_bvdv1_wsv_e1_mapped.nw")
+m_e2 <- read.tree("/Users/gamcil/repos/foldmason-analysis/glycoproteins/trees/mifsud_bvdv1_tdav_e2_mapped.nw")
+m_e2 <- drop.tip(m_e2, c("HPHV_Hepacivirus_sp."))
+
+# FoldMason trees
+#f_e <- read.tree("/Users/gamcil/repos/foldmason-analysis/glycoproteins/trees/foldmason_denv1_e.nw")  # 3Di tree
+f_e <- read.tree("/Users/gamcil/repos/foldmason-analysis/glycoproteins/trees/foldmason_denv1_e_aa.nw")
+f_e1 <- read.tree("/Users/gamcil/repos/foldmason-analysis/glycoproteins/trees/foldmason_bvdv1_wsv_e1.nw")
+f_e2 <- read.tree("/Users/gamcil/repos/foldmason-analysis/glycoproteins/trees/foldmason_bvdv1_tdav_e2.nw")
+
+## Remove OKIAV332/WHYC-1 as in their manuscript
+f_e <- drop.tip(f_e, c("FJJI_Dipteran_jingmen-related_virus_isolate_OKIAV332_segment_2_orfVP1", "FJJI_Wuhan_aphid_virus_1_strain_WHYC-1_segment_2_orfVP1"))
+f_e1 <- drop.tip(f_e1, c("FJJI_Dipteran_jingmen-related_virus_isolate_OKIAV332_segment_2_orfVP1", "FJJI_Wuhan_aphid_virus_1_strain_WHYC-1_segment_2_orfVP1"))
+f_e2 <- drop.tip(f_e2, c("FJJI_Dipteran_jingmen-related_virus_isolate_OKIAV332_segment_2_orfVP1", "FJJI_Wuhan_aphid_virus_1_strain_WHYC-1_segment_2_orfVP1"))
+
+
+# Refolded trees v2
+m_e <- ape::read.tree("/Users/gamcil/Documents/glyco/Mifsud_et_al_Repository/glycoprotein_structural_alignments_and_trees/3di/fn_3di_trees/refolded_fullglyco_E_3di_AA_famsa_parts.nex.treefile")
+m_e <- ape::read.tree("/Users/gamcil/Documents/glyco/Mifsud_et_al_Repository/glycoprotein_structural_alignments_and_trees/3di/fn_3di_trees/refolded_fullglyco_E_3di_famsa_trim35.fas.treefile")
+m_e1 <- ape::read.tree("/Users/gamcil/Documents/glyco/Mifsud_et_al_Repository/glycoprotein_structural_alignments_and_trees/3di/fn_3di_trees/refolded_fullglyco_E1_3di_AA_famsa_parts.nex.treefile")
+m_e2 <- ape::read.tree("/Users/gamcil/Documents/glyco/Mifsud_et_al_Repository/glycoprotein_structural_alignments_and_trees/3di/fn_3di_trees/refolded_fullglyco_E2_3di_AA_famsa_parts.nex.treefile")
+
+f_e <- ape::read.tree("/Users/gamcil/Documents/glyco/mifsud_refolded_foldmason/E_aa_trim35.fa.treefile")
+f_e1 <- ape::read.tree("/Users/gamcil/Documents/glyco/mifsud_refolded_foldmason/E1_aa_trim35.fa.treefile")
+f_e2 <- ape::read.tree("/Users/gamcil/Documents/glyco/mifsud_refolded_foldmason/E2_aa_trim35.fa.treefile")
+
+f_e <- ape::read.tree("/Users/gamcil/Documents/glyco/mifsud_refolded_foldmason/E_partitions.nex.treefile")
+f_e1 <- ape::read.tree("/Users/gamcil/Documents/glyco/mifsud_refolded_foldmason/E1_partitions.nex.treefile")
+f_e2 <- ape::read.tree("/Users/gamcil/Documents/glyco/mifsud_refolded_foldmason/E2_partitions.nex.treefile")
+
+
+
+# Function to find matching nodes and extract bootstrap values
 find_matching_nodes <- function(tree1, tree2) {
+  # Read the trees
+  # tree1 <- read.tree(tree1_path)
+  # tree2 <- read.tree(tree2_path)
+  
   # Ensure both trees have the same taxa
   if (!setequal(tree1$tip.label, tree2$tip.label)) {
     stop("The trees do not have the same set of taxa.")
@@ -35,13 +74,17 @@ find_matching_nodes <- function(tree1, tree2) {
     Tree2_Node = numeric(0),
     Tree2_Bootstrap = numeric(0)
   )
+  
   for (node1 in matching_nodes) {
     node1_label <- tree1_node_labels[[node1]]
     node2 <- which(sapply(tree2_node_labels, function(node2) identical(node1_label, node2)))
+    
     if (length(node2) > 0) {
       node2 <- node2[1]
+      
       bootstrap1 <- tree1$node.label[node1]
       bootstrap2 <- tree2$node.label[node2]
+      
       matching_nodes_info <- rbind(matching_nodes_info, data.frame(
         Tree1_Node = node1,
         Tree1_Bootstrap = as.numeric(bootstrap1),
@@ -54,7 +97,6 @@ find_matching_nodes <- function(tree1, tree2) {
   return(matching_nodes_info)
 }
 
-# Calculates average bootstrap over entire tree
 calculate_mean_tree_support <- function(tree) {
   values <- sapply(1:tree$Nnode, function(i) {
     return(as.numeric(tree$node.label[i]))
@@ -62,7 +104,6 @@ calculate_mean_tree_support <- function(tree) {
   return(mean(values, na.rm=T)) 
 }
 
-# Calculates bootstrap similarity between nodes in two trees
 calculate_matching_node_stats <- function(a, b) {
   matchingNodes <- find_matching_nodes(a, b) %>%
     mutate(difference = Tree1_Bootstrap - Tree2_Bootstrap)
@@ -81,7 +122,8 @@ calculate_matching_node_stats <- function(a, b) {
   return(stats)
 }
 
-# Computes tree similarity metrics between two trees. Split metrics = RF
+
+# Split metrics = RF
 compute_scores <- function(treeA, treeB) {
   quartets <- QuartetStatus(treeA, treeB)
   splits   <- SplitStatus(treeA, treeB)
@@ -92,20 +134,8 @@ compute_scores <- function(treeA, treeB) {
   return(combined) 
 }
 
-# Load Mifsud et al. trees
-# E  = Mifsud_et_al_Repository/glycoprotein_structural_alignments_and_trees/3di/fn_3di_trees/refolded_fullglyco_E_3di_famsa_trim35.fas.treefile
-# E1 = Mifsud_et_al_Repository/glycoprotein_structural_alignments_and_trees/3di/fn_3di_trees/refolded_fullglyco_E1_3di_AA_famsa_parts.nex.treefile
-# E2 = Mifsud_et_al_Repository/glycoprotein_structural_alignments_and_trees/3di/fn_3di_trees/refolded_fullglyco_E2_3di_AA_famsa_parts.nex.treefile
-m_e  <- ape::read.tree("trees/refolded_fullglyco_E_3di_famsa_trim35.fas.treefile")
-m_e1 <- ape::read.tree("trees/refolded_fullglyco_E1_3di_AA_famsa_parts.nex.treefile")
-m_e2 <- ape::read.tree("trees/refolded_fullglyco_E2_3di_AA_famsa_parts.nex.treefile")
 
-# Load trees built from FoldMason MSTAs
-f_e  <- ape::read.tree("trees/E_aa_trim35.fa.treefile")
-f_e1 <- ape::read.tree("trees/E1_aa_trim35.fa.treefile")
-f_e2 <- ape::read.tree("trees/E2_aa_trim35.fa.treefile")
 
-# Collate matched node similarities for each tree -> node_stats
 e_node_stats <- calculate_matching_node_stats(f_e, m_e) %>% mutate(tree="E")
 e1_node_stats <- calculate_matching_node_stats(f_e1, m_e1) %>% mutate(tree="E1")
 e2_node_stats <- calculate_matching_node_stats(f_e2, m_e2) %>% mutate(tree="E2")
@@ -113,7 +143,36 @@ node_stats <- rbind(e_node_stats, e1_node_stats)
 node_stats <- rbind(node_stats, e2_node_stats)
 node_stats
 
-# Compute similarity metrics between each tree pair
+mifsud_e <- read.tree("/Users/gamcil/repos/foldmason-analysis/glycoproteins/seq/refolded_fullglyco_E_3di_famsa_trim35.fas.treefile")
+mifsud_e1 <- read.tree("/Users/gamcil/repos/foldmason-analysis/glycoproteins/seq/refolded_fullglyco_E1_3di_famsa_trim35.fas.treefile")
+mifsud_e2 <- read.tree("/Users/gamcil/repos/foldmason-analysis/glycoproteins/seq/refolded_fullglyco_E2_3di_famsa_trim35.fas.treefile")
+clustal_e <- read.tree("/Users/gamcil/repos/foldmason-analysis/glycoproteins/seq/E_trimal.fa.treefile")
+clustal_e1 <- read.tree("/Users/gamcil/repos/foldmason-analysis/glycoproteins/seq/E1_trimal.fa.treefile")
+clustal_e2 <- read.tree("/Users/gamcil/repos/foldmason-analysis/glycoproteins/seq/E2_trimal.fa.treefile")
+compute_scores(mifsud_e, clustal_e)
+compute_scores(mifsud_e1, clustal_e1)
+compute_scores(mifsud_e2, clustal_e2)
+
+e_node_stats <- calculate_matching_node_stats(clustal_e, mifsud_e) %>% mutate(tree="E")
+e1_node_stats <- calculate_matching_node_stats(f_e1, m_e1) %>% mutate(tree="E1")
+e2_node_stats <- calculate_matching_node_stats(f_e2, m_e2) %>% mutate(tree="E2")
+node_stats <- rbind(e_node_stats, e1_node_stats)
+node_stats <- rbind(node_stats, e2_node_stats)
+node_stats
+
+
+
+f_e <- read.tree()  # 3Di tree
+# f_e <- read.tree("trees/foldmason_denv1_e_aa.nw")
+f_e1 <- read.tree("trees/foldmason_bvdv1_wsv_e1.nw")
+f_e2 <- read.tree("trees/foldmason_bvdv1_tdav_e2.nw")
+
+## Remove OKIAV332/WHYC-1 as in their manuscript
+f_e <- drop.tip(f_e, c("FJJI_Dipteran_jingmen-related_virus_isolate_OKIAV332_segment_2_orfVP1", "FJJI_Wuhan_aphid_virus_1_strain_WHYC-1_segment_2_orfVP1"))
+f_e1 <- drop.tip(f_e1, c("FJJI_Dipteran_jingmen-related_virus_isolate_OKIAV332_segment_2_orfVP1", "FJJI_Wuhan_aphid_virus_1_strain_WHYC-1_segment_2_orfVP1"))
+f_e2 <- drop.tip(f_e2, c("FJJI_Dipteran_jingmen-related_virus_isolate_OKIAV332_segment_2_orfVP1", "FJJI_Wuhan_aphid_virus_1_strain_WHYC-1_segment_2_orfVP1"))
+
+# Compute all similarity metrics
 # Quartet similarity = first row QuartetDivergence
 # Robinson-Foulds ≈ second row (split) SymmetricDifference
 compute_scores(m_e, f_e)
